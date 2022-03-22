c36aa96ad89ec472dc443f76e80112222deaa8b3
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2022-03-22 16:15:22 -0300

    feat(contract): initial setup

---
+++ contracts/MyEpicTripNFT.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.1;
+
+// We first import some OpenZeppelin Contracts.
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
+
+// We inherit the contract we imported. This means we'll have access
+// to the inherited contract's methods.
+contract MyEpicNFT is ERC721URIStorage {
+    // Magic given to us by OpenZeppelin to help us keep track of tokenIds.
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    // We need to pass the name of our NFTs token and its symbol.
+    constructor() ERC721("SquareNFT", "SQUARE") {
+        console.log("This is my NFT Epic Trips contract. Woah!");
+    }
+
+    // A function our user will hit to get their NFT.
+    function makeAnEpicNFT() public {
+        // Get the current tokenId, this starts at 0.
+        uint256 newItemId = _tokenIds.current();
+
+        // Actually mint the NFT to the sender using msg.sender.
+        _safeMint(msg.sender, newItemId);
+        console.log(
+            "An NFT w/ ID %d has been minted to %s",
+            newItemId,
+            msg.sender
+        );
+        // Set the NFTs data.
+        _setTokenURI(newItemId, "https://jsonkeeper.com/b/D3ZI");
+
+        // Increment the counter for when the next NFT is minted.
+        _tokenIds.increment();
+    }
+}
---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.1",
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};

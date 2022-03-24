d9f054c0f300695a0570eba4c831bcd3d826a293
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2022-03-24 15:20:14 -0300

    feat(reactapp): connect wallet

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,17 +1,22 @@
-import './styles/App.css';
-import twitterLogo from './assets/twitter-logo.svg';
+import "./styles/App.css";
+import twitterLogo from "./assets/twitter-logo.svg";
 import React from "react";
+import { useConnectWallet } from "./hooks/use-connect-wallet.hook";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = '';
+const OPENSEA_LINK = "";
 const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
+  const { connectWallet, currentAccount } = useConnectWallet();
   // Render Methods
   const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
       Connect to Wallet
     </button>
   );
@@ -24,7 +29,13 @@ const App = () => {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {renderNotConnectedContainer()}
+          {currentAccount == null ? (
+            renderNotConnectedContainer()
+          ) : (
+            <button onClick={null} className="cta-button connect-wallet-button">
+              Mint NFT
+            </button>
+          )}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
